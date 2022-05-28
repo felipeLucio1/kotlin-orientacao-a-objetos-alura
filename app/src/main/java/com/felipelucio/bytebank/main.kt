997b77d723ef5e651bf26a6744191e6b9e81be3b
felipe lucio <felipelucio2009@gmail.com>
2022-05-27 23:08:38 -0300

    feature: Mostra a diferença entre val e var na linguagem kotlin

--- app/src/main/java/com/felipelucio/bytebank/main.kt
+++ app/src/main/java/com/felipelucio/bytebank/main.kt
@@ -3,5 +3,18 @@ package com.felipelucio.bytebank
 fun main() {
     println("Bem vindo ao Bytebank")
     val titular = "Felipe"
-    println("Olá, $titular")
+    val numero = 1234
+    var saldo = 0.0
+
+    println("titular $titular")
+    println("numero da conta $numero")
+    println("saldo $saldo")
+
+    saldo = 15.toDouble()
+    println("")
+    println("saldo $saldo")
+
+    saldo += 100.0
+    println("")
+    println("saldo $saldo")
 }
