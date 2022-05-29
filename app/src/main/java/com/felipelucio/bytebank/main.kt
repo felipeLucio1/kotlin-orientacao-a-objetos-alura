cf2556b054e00ee96021d39919ee69cc9b840c22
Felipe Lucio do Nascimento <felipelucio2009@gmail.com>
2022-05-28 21:09:14 -0300

    Feature/aula 2: Controle de fluxo e o paradigma procedural (#2)
    * feature: Introduz blocos de controles de fluxo
    
    * refactor: Extrai condicionais para funcao
    
    * feature: Lista diferentes tipos de loops

--- app/src/main/java/com/felipelucio/bytebank/main.kt
+++ app/src/main/java/com/felipelucio/bytebank/main.kt
@@ -2,19 +2,61 @@ package com.felipelucio.bytebank
 
 fun main() {
     println("Bem vindo ao Bytebank")
-    val titular = "Felipe"
-    val numero = 1234
-    var saldo = 0.0
 
-    println("titular $titular")
-    println("numero da conta $numero")
-    println("saldo $saldo")
+    for (i in 1..5) {
+        val titular = "Felipe $i"
+        val numero = 1000+i
+        var saldo = i+10.0
 
-    saldo = 15.toDouble()
-    println("")
-    println("saldo $saldo")
+        println("titular $titular")
+        println("numero da conta $numero")
+        println("saldo $saldo")
+        println()
+    }
 
-    saldo += 100.0
-    println("")
-    println("saldo $saldo")
+    for (i in 5 downTo 1 step 2) {
+        val titular = "Felipe $i"
+        val numero = 1000+i
+        var saldo = i+10.0
+
+        println("titular $titular")
+        println("numero da conta $numero")
+        println("saldo $saldo")
+        println()
+    }
+
+    var i = 0
+    while (i < 5) {
+        val titular = "Felipe $i"
+        val numero = 1000+i
+        var saldo = i+10.0
+
+        println("titular $titular")
+        println("numero da conta $numero")
+        println("saldo $saldo")
+        println()
+        i++
+    }
+    
+//    testaCondicoes(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
+    var saldo1 = saldo
+    if (saldo1 > 0.0) {
+        println("Conta positiva")
+    } else if (saldo1 == 0.0) {
+        println("Conta neutra")
+    } else {
+        println("Conta negativa")
+    }
+
+    saldo1 -= 1000.0
+
+    // o when do kotlin tem a mesma sisgnificado do if {...} else if {...} ... else {...}
+    when {
+        saldo1 > 0.0 -> println("Conta positiva")
+        saldo1 == 0.0 -> println("Conta neutra")
+        else -> println("Conta negativa")
+    }
 }
