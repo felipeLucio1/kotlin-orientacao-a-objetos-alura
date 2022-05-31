4eb1264aa4cb12bddc27e262d1abda595acb1962
Felipe Lucio do Nascimento <felipelucio2009@gmail.com>
2022-05-30 23:45:02 -0300

    feature: Conhecendo a orientacao a objetos (#3)
    * refactor: Extrai looops para uma funcao
    
    * feature: Cria a classe Conta
    
    * feature: Expoe conceito de copia e referencia
    
    * refactor: Extrai teste de copia e referencia para funcao
    
    * feature: Adiciona funcao deposita

--- app/src/main/java/com/felipelucio/bytebank/main.kt
+++ app/src/main/java/com/felipelucio/bytebank/main.kt
@@ -3,6 +3,61 @@ package com.felipelucio.bytebank
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val contaFelipe = Conta()
+    contaFelipe.titular = "Felipe"
+    contaFelipe.numero = 1000
+    contaFelipe.saldo = 100.0
+
+    println("titular ${contaFelipe.titular}")
+    println("numero ${contaFelipe.numero}")
+    println("saldo ${contaFelipe.saldo}")
+
+    val contaRay = Conta()
+    contaRay.titular = "Ray"
+    contaRay.numero = 1001
+    contaRay.saldo = 300.0
+
+    println("titular ${contaRay.titular}")
+    println("numero ${contaRay.numero}")
+    println("saldo ${contaRay.saldo}")
+
+    deposita(contaFelipe, 10.0)
+    deposita(contaRay, 10.0)
+
+    println("saldo Felipe ${contaFelipe.saldo}")
+    println("saldo Ray ${contaRay.saldo}")
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+}
+
+fun testaCopiaEReferencia() {
+    println()
+    println("----------Teste de copia e referencia----------")
+
+    val varX = 10
+    var varY = varX
+    println("varX $varX")
+    println("varY $varY")
+
+    val contaUm = Conta()
+    contaUm.titular = "Um"
+
+    val contaDois = contaUm
+    contaDois.titular = "Dois"
+
+    println("contaUm.titular ${contaUm.titular}")
+    println("contaDois.titular ${contaDois.titular}")
+}
+
+fun testaLacos() {
     for (i in 1..5) {
         val titular = "Felipe $i"
         val numero = 1000+i
@@ -37,8 +92,6 @@ fun main() {
         println()
         i++
     }
-    
-//    testaCondicoes(saldo)
 }
 
 fun testaCondicoes(saldo: Double) {
